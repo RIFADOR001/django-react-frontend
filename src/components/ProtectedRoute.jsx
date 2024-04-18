d90694b4d9dc2d07f567ad2838f9f051c7b46f80
Alejandro Mendez Rojas <amr@Alejandros-MacBook-Pro.local>
2024-04-18 14:03:28 -0600

    Frontend part is done

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -9,14 +9,14 @@ import { useState, useEffect } from "react";
 // The idea here is to prevent access to routes that require authorization
 
 function ProtectedRoute({children}) {
-	const [isAuthorized, setIsAuthorized] = useState(null)
+	const [isAuthorized, setIsAuthorized] = useState(null);
 
 	useEffect(() => {
 		auth().catch(() => setIsAuthorized(false))
 	}, [])
 
 	const refreshToken = async () => {
-		const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+		const refreshToken = localStorage.getItem(REFRESH_TOKEN);
 		try {
 			// This automatically uses the baseURL from api.js
 			const res = await api.post("/api/token/refresh/", {
@@ -24,42 +24,44 @@ function ProtectedRoute({children}) {
 			});
 			if (res.status === 200) {
 				localStorage.setItem(ACCESS_TOKEN. res.data.access)
-				setIsAuthorized(true)
+				setIsAuthorized(true);
 			} else {
-				setIsAuthorized(false)
+				setIsAuthorized(false);
 			}
 		} catch (error) {
 			console.log(error)
-			setIsAuthorized(false)
+			setIsAuthorized(false);
 		}
 	}
 
 	const auth = async () => {
-		const token = localStorage.getItem(ACCESS_TOKEN)
-		if (!token) {
-			setIsAuthorized(false)
-			return
-		}
-		const decoded = jwtDecode(token)
-		const tokenExpiration = decode.exp
-		// By default this is in miliseconds, we change to seconds
-		const now = Date.now() / 1000
+		const token = localStorage.getItem(ACCESS_TOKEN);
 
+		if (!token) {
+			setIsAuthorized(false);
+			return;
+		}
+
+		const decoded = jwtDecode(token);
+		const tokenExpiration = decoded.exp;
+		// By default this is in miliseconds, we change to seconds
+		const now = Date.now() / 1000;
 		if (tokenExpiration < now) {
 			await refreshToken()
 		} else {
-			setIsAuthorized(true)
+			setIsAuthorized(true);
 		}
 
 	}
 
 	if (isAuthorized === null) {
-		return <div>Loading...</div>
+		return <div>Loading...</div>;
 	}
+
 
 	// This should take us automatically to the page if it is authorized and
 	// to login if not
-	return isAuthorized ? children : <Navigate to="/login" />
+	return isAuthorized ? children : <Navigate to="/login" />;
 }
 
 
